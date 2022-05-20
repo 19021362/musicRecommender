4f28c5cfd55ac095741b9c2c7d758c7c569d14c9
19021362 <19021362@vnu.edu.vn>
2022-05-20 19:01:06 +0700

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
+from smtpd import MailmanProxy
+from tkinter import Frame
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render
 from matplotlib.style import context
 from .models import *
 from .mlmodel import *
+
 import os
 
 from scipy.spatial.distance import cdist
@@ -13,29 +16,41 @@ import pickle
 import numpy as np 
 import pandas as pd 
 from django.shortcuts import render
-from django.contrib import messages 
+from django.contrib import messages
+
+from app import mlmodel 
 
 def sample_detail(request, sample_id):
     song_name_list = Song_Sample.objects.filter(sample_id = sample_id)
     year_list = []
-    # song_list = []
-    # for i in song_name_list:
-    #     song_list.append(Song.objects.filter(id = i.song_id))
-    # print(song_list)
-    # for i in song_list:
-    #     year_list.append(i.get_year())
-    # print(song_name_list)
+
+    song_play_id = find_song_id(name=song_name_list[0])
+    #print(song_play_id)
+    song_name_list = find_song_list_id(song_list=song_name_list)
+
+    url = "https://open.spotify.com/embed/track/" + song_play_id + "?utm_source=generator"
+    #print(url)
+    
     context = {
         'song_list': song_name_list,
-        'year_list': year_list
+        'year_list': year_list,
+        'url' : url
     }
     return render(request, 'index.html', context)
 
-data = pd.read_csv("/home/tieu/Documents/musicRecommender/app/data.csv")
+
+data = pd.read_csv("app/tracks_features.csv")
+
 
 def recommendation_detail(request, sample_id):
-    recommender = pickle.load(open('/home/tieu/Documents/musicRecommender/app/mlmodel.sav','rb'))
+    recommender = pickle.load(open('app/mlmodel.sav','rb'))
     song_list = Song_Sample.objects.filter(sample_id = sample_id)
+    year_list = []
+    print(song_list[0])
+    song_play_id = find_song_id(name=song_list[0])
+    url = "https://open.spotify.com/embed/track/" + song_play_id + "?utm_source=generator"
+
+    song_list_id = find_song_list_id(song_list=song_list)
     n_songs = 10
     
     metadata_cols = ['name', 'year', 'artists']
@@ -49,9 +64,30 @@ def recommendation_detail(request, sample_id):
     index = list(np.argsort(distances)[:, :n_songs][0])
     
     rec_songs = data.iloc[index]
-    # rec_songs = rec_songs[~rec_songs['name'].isin(song_dict['name'])]
     rec_songs = rec_songs[~rec_songs['name'].isin(song_list)]
 
+    context = {
+        'song_list' : song_list_id,
+        'year_list' : year_list,
+        'recommend_list' : rec_songs['name'],
+        'url' : url
+    }
+
     # return render(request, 'index.html', {'recommend_list':rec_songs[metadata_cols].to_dict(orient='records')})
-    return render(request, 'index.html', {'recommend_list':rec_songs['name']})
+    return render(request, 'index.html', context)
     
+
+def choose_song(request, sample_id, id):
+    song_name_list = Song_Sample.objects.filter(sample_id = sample_id)
+    year_list = []
+
+    song_name_list = find_song_list_id(song_list=song_name_list)
+
+    url = "https://open.spotify.com/embed/track/" + id + "?utm_source=generator"
+    
+    context = {
+        'song_list': song_name_list,
+        'year_list': year_list,
+        'url' : url
+    }
+    return render(request, 'index.html', context)
