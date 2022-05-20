b77c4c1cf0c21184a0bf06fc5c426a12da358d53
19021362 <19021362@vnu.edu.vn>
2022-05-21 00:06:21 +0700

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    #path('', views.index, name='index'),
+    path('', views.home, name='home'),
     #path('getdata/', views.get_data, name='get_data'),
     path('<int:sample_id>/', views.recommendation_detail, name='detail'),
     #path('<int:sample_id>/recommendation', views.recommendation_detail, name='recommendation_detail'),
