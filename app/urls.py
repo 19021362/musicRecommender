c38a1648c6fbe69d59890510914f9cf0868bb71e
19021362 <19021362@vnu.edu.vn>
2022-05-22 01:21:14 +0700

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     #path('getdata/', views.get_data, name='get_data'),
-    path('<int:sample_id>/', views.recommendation_detail, name='detail'),
+    path('sample/<int:sample_id>/', views.recommendation_detail, name='detail'),
     #path('<int:sample_id>/recommendation', views.recommendation_detail, name='recommendation_detail'),
-    path('<int:sample_id>/<str:id>', views.choose_song, name='choose_song')
+    path('track/<str:id>/', views.choose_song, name='choose_song'),
+    path('add/<str:id>/', views.add_song_playlist, name='add_song'),
+    path('remove/<str:id>/', views.remove_song_playlist, name='remove_song')
 ]
